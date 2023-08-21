0f418a26e22fa81627823ae1d07669dee18b8931
ramoji4b5 <ramojirao999@gmail.com>
2023-08-21 15:43:32 +0530

    gcp_tf_lab1

---
+++ tf_gcp/variable.tf
@@ -0,0 +1,21 @@
+variable "gcp_tf_bucket" {
+  default = "ramoji_tf_01"
+}
+
+# variable "gcp_tf_project" {
+
+# }
+
+variable "gcp_tf_zone" {
+  default = "us-central1-a"
+
+}
+
+variable "gcp_tf_region" {
+  default = "US"
+}
+
+variable "gcp_tf_instance" {
+  default = "e2-medium"
+
+}
